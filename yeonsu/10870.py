3b3a075d28bf38807ebae560f1aa69e8f82338c2
Elle <elle@Elleui-MacBookPro.local>
2024-07-01 15:18:26 +0900

    Docs : 피보나치 / 10분

--- yeonsu/10870.py
+++ yeonsu/10870.py
@@ -1,3 +1,15 @@
+# 원래 푼 코드 (재귀함수) --- 런타임 에러, 넘 오래걸림
+n = int(input())
+def fibo(n):
+    if n==1 or n==2:
+        return 1
+    else :
+        return fibo(n-1) + fibo(n-2)
+
+print(fibo(n))
+
+
+# 반복문으로 다시 풀었음
 n = int(input())
 array = []
 
