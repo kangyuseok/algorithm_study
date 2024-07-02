9807477abe19f8b56b11a0d840a6321ce767ed67
whdmldus1116 <76983867+whdmldus1116@users.noreply.github.com>
2024-07-02 16:48:04 +0900

    피보나치의 수 5

---
+++ lily/boj_10870.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_10870 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[num + 1];
+
+        arr[0] = 0;
+        arr[1] = 1;
+
+        for(int i=2; i<num + 1; i++){
+            arr[i] = arr[i-2] + arr[i-1];
+        }
+        System.out.println(arr[num]);
+    }
+}
